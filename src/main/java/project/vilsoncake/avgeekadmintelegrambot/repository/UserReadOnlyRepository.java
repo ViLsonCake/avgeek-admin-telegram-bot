336603932679377b897410daf693c47ed731050e
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-08-14 17:55:12 +0200

    added read-only repository

--- src/main/java/project/vilsoncake/avgeekadmintelegrambot/repository/UserReadOnlyRepository.java
+++ src/main/java/project/vilsoncake/avgeekadmintelegrambot/repository/UserReadOnlyRepository.java
@@ -1,9 +1,8 @@
 package project.vilsoncake.avgeekadmintelegrambot.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import project.vilsoncake.avgeekadmintelegrambot.entity.UserEntity;
 
 import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<UserEntity, UUID> {
+public interface UserReadOnlyRepository extends ReadOnlyRepository<UserEntity, UUID> {
 }
